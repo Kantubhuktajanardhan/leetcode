125ae4651b8795163cb5abf6476df954e3e6c690
Janardhan Kantubhukta <75320426+Kantubhuktajanardhan@users.noreply.github.com>
2024-01-27 17:14:50 +0530

    Create Missing Number.java

---
+++ Missing Number.java
@@ -0,0 +1,54 @@
+/*
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+*/
+
+class Solution {
+    void cyclicsort(int nums[])
+    {
+        int i=0;
+        while(i<nums.length)
+        {
+            if(nums[i]< nums.length &&nums[i] != nums[nums[i]])
+            {
+                //swap
+                int temp=nums[nums[i]];
+                nums[nums[i]]=nums[i];
+                nums[i]=temp;
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+    public int missingNumber(int[] nums) {
+        cyclicsort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i] != i)
+            {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
